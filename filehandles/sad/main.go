6de26aa4f3885f5c7ad88ba5af4c36d0aa97febf
Nick Sanford <nick.sanford@viam.com>
2024-01-17 14:58:50 -0500

    wip

--- filehandles/sad/main.go
+++ filehandles/sad/main.go
@@ -6,17 +6,24 @@ import (
 	"path"
 )
 
-const NUM_FILES = 10000000000
+const NUM_FILES = 1 //0000000000
 
 func main() {
 	tmpDir := os.TempDir()
 	log.Printf("tempdir: %s", tmpDir)
-	for i := 0; i < NUM_FILES; i++ {
-		go func() {
-			_, err := os.CreateTemp(path.Join(tmpDir, "resource-exhaustion"), "")
-			if err != nil {
-				log.Fatalf(err.Error())
-			}
-		}()
+	if err := os.MkdirAll(path.Join(tmpDir, "resource-exhaustion"), 0o650); err != nil {
+		log.Fatalf(err.Error())
 	}
+	_, err := os.CreateTemp(path.Join(tmpDir, "resource-exhaustion"), "")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	// for i := 0; i < NUM_FILES; i++ {
+	// 	go func() {
+	// 		_, err := os.CreateTemp(path.Join(tmpDir, "resource-exhaustion"), "")
+	// 		if err != nil {
+	// 			log.Fatalf(err.Error())
+	// 		}
+	// 	}()
+	// }
 }
