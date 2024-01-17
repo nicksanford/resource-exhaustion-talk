cb3341e0c5b2a64dab3affe157becbbe14013d38
Nick Sanford <nick.sanford@viam.com>
2024-01-17 15:14:10 -0500

    wip

--- filehandles/sad/main.go
+++ filehandles/sad/main.go
@@ -6,24 +6,33 @@ import (
 	"path"
 )
 
-const NUM_FILES = 1 //0000000000
+const NUM_FILES = 10000000000
 
 func main() {
 	tmpDir := os.TempDir()
 	log.Printf("tempdir: %s", tmpDir)
-	if err := os.MkdirAll(path.Join(tmpDir, "resource-exhaustion"), 0644); err != nil {
+	p := path.Join(tmpDir, "resource-exhaustion")
+	log.Print(p)
+
+	if err := os.RemoveAll(p); err != nil {
 		log.Fatalf(err.Error())
 	}
+
+	if err := os.MkdirAll(p, 0o755); err != nil {
+		log.Fatalf(err.Error())
+	}
+
 	_, err := os.CreateTemp(path.Join(tmpDir, "resource-exhaustion"), "")
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
-	// for i := 0; i < NUM_FILES; i++ {
-	// 	go func() {
-	// 		_, err := os.CreateTemp(path.Join(tmpDir, "resource-exhaustion"), "")
-	// 		if err != nil {
-	// 			log.Fatalf(err.Error())
-	// 		}
-	// 	}()
-	// }
+
+	for i := 0; i < NUM_FILES; i++ {
+		go func() {
+			_, err := os.CreateTemp(path.Join(tmpDir, "resource-exhaustion"), "")
+			if err != nil {
+				log.Fatalf(err.Error())
+			}
+		}()
+	}
 }
