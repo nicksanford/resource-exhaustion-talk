dcbe2911dcdf3416790ac2c1583130108848cbf0
Nick Sanford <nick.sanford@viam.com>
2024-01-17 15:31:54 -0500

    wip

--- filehandles/sad/main.go
+++ filehandles/sad/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path"
+	"strings"
 )
 
 const NUM_FILES = 10000000000
 
 func main() {
+	log.Printf("PID: %d\n", os.Getpid())
+
+	var start string
+	for {
+		log.Println("start? y/n")
+		fmt.Scan(&start)
+		start = strings.ToUpper(start)
+		if start == "Y" {
+			break
+		}
+
+		if start == "N" {
+			return
+		}
+	}
+
 	tmpDir := os.TempDir()
-	log.Printf("tempdir: %s", tmpDir)
+	log.Printf("tempdir: %s\n", tmpDir)
 	p := path.Join(tmpDir, "resource-exhaustion")
-	log.Print(p)
+	log.Println(p)
 
 	if err := os.RemoveAll(p); err != nil {
 		log.Fatalf(err.Error())
@@ -28,11 +46,11 @@ func main() {
 	}
 
 	for i := 0; i < NUM_FILES; i++ {
-		go func() {
-			_, err := os.CreateTemp(path.Join(tmpDir, "resource-exhaustion"), "")
-			if err != nil {
-				log.Fatalf(err.Error())
-			}
-		}()
+		// go func() {
+		_, err := os.CreateTemp(path.Join(tmpDir, "resource-exhaustion"), "")
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		// }()
 	}
 }
