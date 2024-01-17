f2d68b95f6a0dd09dd7c8d07975e789f92f03d57
Nick Sanford <nick.sanford@viam.com>
2024-01-17 14:01:56 -0500

    first commit

--- filehandles/sad/main.go
+++ filehandles/sad/main.go
@@ -9,7 +9,6 @@ const NUM_FILES = 10000000000
 
 func main() {
 	log.Printf("tempdir: %s", os.TempDir())
-
 	for i := 0; i < NUM_FILES; i++ {
 		_, err := os.CreateTemp("", "resource-exhaustion")
 		if err != nil {
